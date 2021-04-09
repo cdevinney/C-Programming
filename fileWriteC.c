ac1610903920c3c11faeb41cab88f1df65d03240
cdevinney <cedevinney@gmail.com>
2021-04-09 08:56:10 -0400

    Adding file write elements

--- fileWriteC.c
+++ fileWriteC.c
@@ -5,19 +5,27 @@ int main()
 {
     int n;
     char *text;
+    FILE *fp;
 
     printf("Enter limit of the text:\n");
     scanf("%d", &n);
-     text=(char*)malloc(n*sizeof(char));
+    text=(char*)malloc(n*sizeof(char));
 
     printf("Please type what you would like written to the file. When finished, hit 'Enter' and type 'EXIT'.\n");
-    scanf(" ");
-    gets(text);
-
-    printf("%s\n", text);
-
-    free(text);
-
-    return 0;
     
+    FILE *fopen(const char * fileWritingInC, const char * a);
+
+    while (text != "EXIT")
+    {
+        scanf(" ");
+        gets(text);
+        fprintf(fp, text);
+        free(text);
+    }
+
+    int fclose(FILE *fp);
+
+    printf("Your file, fileWritingInC.txt, has been written.\n");
+    
+    return 0; 
 }
